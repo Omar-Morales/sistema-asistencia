136ad1327041d66916f783f4e887e9d4db551572
Omar Morales <omaromarmorales2020@gmail.com>
2025-10-01 02:00:31 -0500

    actualizacion mysql

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion= new mysqli("localhost","root","","sistema_asistencia","3306");
+$conexion= new mysqli("localhost","root","admin","sistema_asistencia","3306");
 $conexion->set_charset("utf8");
 date_default_timezone_set("America/Lima");
 ?>
